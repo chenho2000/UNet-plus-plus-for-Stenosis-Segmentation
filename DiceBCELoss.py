5bf5f79c0f7a9445920ddeb02a243846a7c18f73
Chen <tommytime2000@outlook.com>
2025-04-10 00:46:45 -0400

    update training notebook and utils for model evaluation metrics

---
+++ DiceBCELoss.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs, targets, smooth=1e-7):
+        inputs = F.sigmoid(inputs)
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+
+        return Dice_BCE
